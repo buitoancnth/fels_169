8962aa047470732da2b39a9de2b63cd2673bad15
Bui Toan <buitoanncth@gmail.com>
2017-06-09 16:28:11 +0700

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
   get "/help", to: "static_pages#help"
   get "/signup", to: "users#new"
   root "static_pages#home"
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
