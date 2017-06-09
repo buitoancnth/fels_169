410b8f2133cb27ed98e73f7b642d5580f4cacd8f
Bui Toan <buitoanncth@gmail.com>
2017-06-09 16:28:11 +0700

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,:password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "notuser"
+    redirect_to root_path
+  end
+end
