28531bd5f47b2a3ec75467d5a542aca9ae3353c8
Bui Toan <buitoanncth@gmail.com>
2017-06-14 14:43:57 +0700

    category

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,5 @@ class Lesson < ApplicationRecord
   belongs_to :category
   has_many :words, through: :lesson_words, dependent: :destroy
   has_many :answers, through: :lesson_words, dependent: :destroy
+  scope :user_start, -> user_id {where user_id: user_id}
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -6,12 +6,12 @@ class Word < ApplicationRecord
 
   scope :learned, -> user_id do
     where "id in (select word_id from answers where
-    is_correct = '1' and id in (select answer_id from lesson_words where
+    is_correct = 't' and id in (select answer_id from lesson_words where
     lesson_id in (select id from lessons where user_id = #{user_id})))"
   end
   scope :no_learn, -> user_id do
     where "id not in (select word_id from answers where
-    is_correct = '1' and id in (select answer_id from lesson_words where
+    is_correct = 't' and id in (select answer_id from lesson_words where
     lesson_id in (select id from lessons where user_id = #{user_id})))"
   end
 end
